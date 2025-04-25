b6fe248dd92f8eec5bc2d476af32d951a7ae201e
shewag-rk <shewagsurya02gmail.com>
2025-04-25 17:16:38 +0530

    logical-problem

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 // 1.Find the first bigest 
 // 2.second bigest value;
-
 // let array = [20, 90, 50, 12, 3];
 // let firstLargest = 0, secondLargest = 0;
-
 // for(let i = 0; i< array.length; i++){
     // if(array[i] > firstLargest){
         // secondLargest = firstLargest;
@@ -16,10 +14,10 @@
 //         secondLargest = array[i]
 //     }
 // }
-
 // console.log(firstLargest);
 // console.log(secondLargest);
 
+// --------------------------------------------------- //
 
 // 3.Reverse the array:
 // let arr = [1,2,3,4,5,6,7,8,9,10];
@@ -27,6 +25,7 @@
 //     console.log(arr[i]);
 // }
 
+// --------------------------------------------------- //
 
 // 4. Find the vowles in the Array:
 // let str = 'enginerring', char = str.toLowerCase(), count = 0;
@@ -38,57 +37,32 @@
 // }
 // console.log(count);
 
+// --------------------------------------------------- //
 
 // 5. Sort asscending and pick the unique value.
-
-
-function sortNum(arr){
-let flag = false, empty = []
-for(let i=0; i<arr.length; i++){
-    for(let j=i+1; j<arr.length; j++){
-        if(arr[i] === arr[j])[
-            arr[j]= '@'
-        ]
-    }
-}
-while(!flag){
-    flag = true;
-    for(let i=0; i<arr.length; i++){
-        if(arr[i-1] > arr[i]){
-            flag = false;
-            let temp = arr[i-1];
-            arr[i-1] = arr[i];
-            arr[i] = temp
-        }
-    }
-}
-for(let i = 0; i<arr.length; i++){
-    if(arr[i] !== '@'){
-        empty[empty.length] = arr[i]
-    }
-}
-return empty
-}
-
-
-let arr = [2,3,2,4,2,1,3,5,6,7];
-console.log(sortNum(arr));
-
-
+// let arr = [2,3,2,4,2,1,3,5,6,7], empty = [], newArr = []
 // for(let i=0; i<arr.length; i++){
-//     // console.log(arr[i]);   
-//     for(let j=0; j<arr[i-1]; j++){
-//         // console.log(arr[j]);
-//         if(arr[i] > arr[j+1]){
-//             temp = arr[j];
-//             arr[j] = arr[j+1];
-//             arr[j+1] = temp
+//     for(let j=i+1; j<arr.length; j++){
+//         if(arr[i] === arr[j]){
+//             arr[j]= '@'
+//         }
 
+//         if(arr[i] > arr[j]){
+//             newArr = arr[i];
+//             arr[i] = arr[j];
+//             arr[j] = newArr
 //         }
 //     }
 // }
-// console.log(temp);
+// for(let i = 0; i<arr.length; i++){
+//     if(arr[i] !== '@'){
+//         empty[empty.length] = arr[i]
+//     }
+// }
 
+// console.log(empty);
+
+// --------------------------------------------------- //
 
 // 6 Finding the missing numbers in the array:
 // function findMissingNumber(arr){
@@ -107,10 +81,21 @@ console.log(sortNum(arr));
 // let array  = [1 ,12 ,10];
 // findMissingNumber(array);
 
+// --------------------------------------------------- //
 
 // 7. Need to count value after the decimal point.
+// let num = 3489.2345, num2 = ''+num , count = 0, temps = 0;
+// console.log(num2);
 
+// for(let i=0; i<num2.length; i++){
+//     if(num2[i] == '.'){
+//         temps = i
+//     }
+// }
+// count = num2.length-temps-1
+// console.log(count);
 
+// --------------------------------------------------- //
 
 // 8. Finding count of max value:
 // let array = [2,5,1,4,2,1,9], newArr = [];
@@ -119,7 +104,7 @@ console.log(sortNum(arr));
 // }
 // console.log(newArr);
 
-
+// --------------------------------------------------- //
 
 // 9. Search string in the array:
 // function searchFunc(arr, searchStr){
@@ -136,6 +121,7 @@ console.log(sortNum(arr));
 // let array = ["orgination", "abb", "hello"];
 // console.log(searchFunc(array, "abb"));
 
+// --------------------------------------------------- //
 
 // 10. Need to count the each string:
 // let obj = {};
@@ -145,19 +131,3 @@ console.log(sortNum(arr));
 //     obj[char] = (obj[char] || 0) + 1;
 // }
 // console.log(obj);
-
-
-
-
-
-// while(!flag){
-//     flag = true;
-//     for(let i=0; i<arr.length; i++){
-//         if(arr[i-1] > arr[i]){
-//             flag = false;
-//             let temp = arr[i-1];
-//             arr[i-1] = arr[i];
-//             arr[i] = temp
-//         }
-//         }
-//     }
